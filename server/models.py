ed6bfbdb54e485e89dbc4e8d75e6f4b23271d7b2
molly <mollynzembi@gmail.com>
2024-06-30 22:48:33 +0300

    created foreign keys

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy import MetaData, ForeignKey, Column, Integer
+from sqlalchemy.orm import relationship, validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -21,8 +21,16 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade='all, delete')
 
     # add serialization rules
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address,
+            'pizzas': [pizza.to_dict() for pizza in self.pizzas]
+        }
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +44,16 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurants = db.relationship('RestaurantPizza', back_populates='pizza', cascade='all, delete')
 
     # add serialization rules
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'ingredients': self.ingredients,
+            'restaurants': [restaurant.to_dict() for restaurant in self.restaurants]
+        }
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -48,12 +64,29 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id', ondelete='CASCADE'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id', ondelete='CASCADE'), nullable=False)
 
     # add relationships
+    restaurant = db.relationship('Restaurant', back_populates='pizzas')
+    pizza = db.relationship('Pizza', back_populates='restaurants')
 
     # add serialization rules
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'price': self.price,
+            'restaurant_id': self.restaurant_id,
+            'pizza_id': self.pizza_id,
+        }
 
     # add validation
+    @validates('price')
+    def validate_price(self, key, price):
+        if price < 0:
+            raise ValueError("Price must be non-negative")
+        return price
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
+
